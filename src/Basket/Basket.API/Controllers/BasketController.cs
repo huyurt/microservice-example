efc56230630eb5cf1de87f33a3274a71ff387c66
Hudayfe Yurt <hudayfeyurt@hotmail.com>
2021-01-04 01:39:05 +0300

    catalog api

---
+++ src/Basket/Basket.API/Controllers/BasketController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Basket.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class BasketController : ControllerBase
+    {
+        private readonly ILogger<BasketController> _logger;
+
+        public BasketController(ILogger<BasketController> logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+    }
+}
